99c4671cf354c12e4587c0b5d8b328c11f06d6fe
tatianajiselle <tjexoxo@gmail.com>
2018-05-02 21:07:59 -0700

    Updated Isomorphic Strings

--- HashTables/IsomorphicStrings.java
+++ HashTables/IsomorphicStrings.java
@@ -2,23 +2,23 @@
 // linear space + time
 
 public static boolean isIsomorphic(String input1, String input2) {
-    if (input1.length() != input2.length()){
-        return false;
-    } else {
-        HashMap<Character, Integer> hm1 = new HashMap<>();
-        HashMap<Character, Integer> hm2 = new HashMap<>();
+    
+    if (input1.length() != input2.length()) return false;
+    
+    Map<Character, Character> freq1 = new HashMap<>();
+    Map<Character, Character> freq2 = new HashMap<>();
+    
+    for (int i=0; i<input1.length(); i++) {
+        char c1 = input1.charAt(i);
+        char c2 = input2.charAt(i);
         
-        for(int i=0; i < input1.length(); i++){
-            Character c1 = input1.charAt(i);
-            Integer count1 = hm1.containsKey(c1) ? hm1.get(c1) : 0;
-            hm1.put(c1,count1+1);
+        if (!freq1.containsKey(c1))
+            freq1.put(c1, c2);
+        i
+        f (!freq2.containsKey(c2))
+            freq2.put(c2, c1);
         
-            Character c2 = input2.charAt(i);
-            Integer count2 = hm2.containsKey(c2) ? hm2.get(c2) : 0;
-            hm2.put(c2,count2+1);
-            
-            if(!hm1.get(c1).equals(hm2.get(c2))) return false;
-        }
-        return true;
+        if (freq1.get(c1) != c2 || freq2.get(c2) != c1) return false;
     }
+    return true;
 }
