408954ad56936c140215a377495a39ea26b85cb3
tatianajiselle <tjexoxo@gmail.com>
2018-05-24 15:17:10 -0700

    added rotate lefT

---
+++ arrays/RotateLeft.java
@@ -0,0 +1,19 @@
+public static int[] rotateLeft(int[] arr, int k)  { 
+        if (arr == null) return null;
+            int actualShifts = k % arr.length;
+                reverse(arr, 0, arr.length-1);
+                reverse(arr, 0, arr.length - actualShifts-1);
+                reverse(arr, arr.length - actualShifts, arr.length-1);
+        return arr;
+}
+ 
+public static void reverse(int[] arr, int left, int right){
+        if(arr == null || arr.length == 1) return;
+        while(left < right){
+             int temp = arr[left];
+             arr[left] = arr[right];
+             arr[right] = temp;
+             left++;
+             right--;
+        }   
+}
